801af524e985bb23ea8fe7b2a11f406973b2a75f
Arsen Muk <arsenmuk@clickhouse.com>
2026-02-06 12:43:58 +0000

    Introduce cache preheat and staleness for Iceberg

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -4107,6 +4107,8 @@ std::shared_ptr<IcebergMetadataFilesCache> Context::getIcebergMetadataFilesCache
 
 void Context::clearIcebergMetadataFilesCache() const
 {
+    LOG_INFO(getLogger("DDDBG"), "Context::clearIcebergMetadataFilesCache");
+
     auto cache = getIcebergMetadataFilesCache();
 
     /// Clear the cache without holding context mutex to avoid blocking context for a long time
